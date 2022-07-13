d9437465a3703af7596470d524cea3b8cdbabba9
Karbonara <37299335+karbonara@users.noreply.github.com>
2022-07-13 12:02:18 +0300

    Правит файл CalculatorStock

--- src/components/calculator/calculator-stock/CalculatorStock.jsx
+++ src/components/calculator/calculator-stock/CalculatorStock.jsx
@@ -1,5 +1,11 @@
 import BoxSmallImg from "../../../assets/img-services/boxSmall.svg";
 import BoxBigImg from "../../../assets/img-services/boxBig.svg";
+import TiresImg from "../../../assets/img-services/tires.svg";
+import BikeImg from "../../../assets/img-services/bake.svg";
+import SkisImg from "../../../assets/img-services/skis.svg";
+import KickScooterImg from "../../../assets/img-services/kickScooter.svg";
+import FurnitureImg from "../../../assets/img-services/furniture.svg";
+import Appliances from "../../../assets/img-services/appliances.svg";
 import { useState } from "react";
 import "./CalculatorStock.scss";
 
@@ -9,6 +15,39 @@ const CalculatorStock = () => {
   const toggleTab = (index) => {
     setToggleState(index)
   };
+
+  const inventory = [
+    {
+      title: 'Шины ',
+      price: 500,
+      img: TiresImg
+    },
+    {
+      title: 'Велосипед',
+      price: 500,
+      img: BikeImg
+    },
+    {
+      title: 'Горные лыжи Сноуборд',
+      price: 200,
+      img: SkisImg
+    },
+    {
+      title: 'Электросамокаты',
+      price: 500,
+      img: KickScooterImg
+    },
+    {
+      title: 'Хранение мебели',
+      price: 1000,
+      img: FurnitureImg
+    },
+    {
+      title: 'Хранение бытовой техники',
+      price: 1000,
+      img: Appliances
+    },
+  ]
 
   return (
     <div>
@@ -40,18 +79,38 @@ const CalculatorStock = () => {
           className={toggleState === 2 ? "content content__active" : "content"}
         >
           <div className="content__boxes">
-            <img src={BoxSmallImg} alt="box" />
-            <div>200 ₽/мес</div>
+            <div className="content__box-img">
+              <span className="content__box-img-text-1">50 см</span>
+              <img src={BoxSmallImg} alt="box" />
+              <span className="content__box-img-text-2">50 см</span>
+            </div>
+            <div className="content__boxes-text">200 ₽/мес</div>
           </div>
           <div className="content__boxes">
-            <img src={BoxBigImg} alt="box" />
-            <div>200 ₽/мес</div>
+            <div className="content__box-img">
+              <span className="content__box-img-text-big-1">100 см</span>
+              <img className="content__box-img-big" src={BoxBigImg} alt="box" />
+              <span className="content__box-img-text-big-2">100 см</span>
+            </div>
+            <div className="content__boxes-text">200 ₽/мес</div>
           </div>
         </div>
         <div
           className={toggleState === 3 ? "content content__active" : "content"}
         >
-
+          <ul className="additional-services__items">
+            {
+              inventory.map((item, index) => (
+                <li className="additional-services__item" key={index}>
+                  <div className="additional-services__item-text-main">
+                    <div className="additional-services__item-text">{item.title}</div>
+                    <div className="additional-services__item-text">{item.price} ₽ </div>
+                  </div>
+                  <img src={item.img} alt={item.title} />
+                </li>
+              ))
+            }
+          </ul>
         </div>
 
       </div>
